abd80ac961e6f00029933f71823037637d81cdfd
radil <adil.r@northeastern.edu>
2022-03-28 17:06:07 -0400

    Set up Raw sockets and connected properly. Make sure to run program as administrator or use sudo in linux

--- project_constants.py
+++ project_constants.py
@@ -1,3 +1,4 @@
 TEST_HOST = "david.choffnes.com"
 TEST_HOST_2 = "ccs.neu.edu"
 TCP_PORT = 80
+DOUBLE_LINE_DIVIDER="================================================================="
--- raw_sockets_package/rawSocket.py
+++ raw_sockets_package/rawSocket.py
@@ -1,24 +1,35 @@
 import socket as S
 from socket import AF_INET, SOCK_RAW, IPPROTO_RAW
+from project_constants import DOUBLE_LINE_DIVIDER
 
 class raw_socket():
 
-    def __init__(self,host_in,port_in):
+    def __init__(self):
+
         self.socket_sender = S.socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
-
-
-        self.socket_sender.connect((host_in,42))
-
-
-
-        try:
-            self.socket_rcvr = S.socket(S.AF_INET, S.SOCK_RAW, S.IPPROTO_TCP)
-        except:
-            print("ERROR: receiver socket could not be created")
+        self.socket_rcvr = S.socket(S.AF_INET, S.SOCK_RAW, S.IPPROTO_TCP)
 
         self.source_ip = None
         self.dest_ip = None
-        self.port = port_in
+        self.port = None
+
+    def set_sender_socket(self,host_name, port_in,display=False):
+        host_in = S.gethostbyname(host_name)
+        try:
+            self.socket_sender.connect((host_in, port_in))
+        except:
+            if display == True:
+                print("ERROR: Socket Sender failed to connect:\n"
+                      f"HOST: {host_name}\nHOST IP: {host_in}\nPORT: {port_in}\n" +
+                      "EXITING PROGRAM")
+            self.close_connection()
+            exit(1)
+
+        if display == True:
+            print(f"Raw TCP Sender Socket Successfully Connected to:"
+                  f"\nHOST: {host_name}\nHOST IP: {host_in}\nPORT: {port_in}\n"+
+                  DOUBLE_LINE_DIVIDER)
+
 
     def set_socket_values(self,src_ip_in, dest_ip_in):
         self.source_ip = src_ip_in
